f9eba59aa3c4e3d544ed76d482633bb58963fc58
takaset16 <78712268+takaset16@users.noreply.github.com>
2021-07-08 17:22:11 +0900

    Delete run.sh

--- LatentDA/run.sh
+++
@@ -1,8 +0,0 @@
-source /etc/profile.d/modules.sh
-module load python/3.6/3.6.5 cuda/10.1/10.1.243 cudnn/7.6/7.6.5 nccl/2.5/2.5.6-1 openmpi/2.1.6 gcc/7.4.0
-source ~/venv/pytorch/bin/activate
-
-
-python main.py --n_model 'CNN' --n_data 'CIFAR-10' --num_epochs 10 --flag_myaug_training 1 --layer_aug 0 --flag_random_layer 1 --n_aug 1 --flag_als 1 --als_rate 0.001 --epoch_random 0 --gpu_multi 0 --loop 0
-
-deactivate
